8784713b31f324ce81c06baced1d3b50ca62348d
camel113 <adrienbigler@gmail.com>
2019-10-01 14:55:32 +0200

    Refactor code :-)

--- arangeIrregularPlanDetection/ViewController.swift
+++ arangeIrregularPlanDetection/ViewController.swift
@@ -14,67 +14,52 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    private var planeGeometry: ARSCNPlaneGeometry!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
+        let device = MTLCreateSystemDefaultDevice()!
+        planeGeometry = ARSCNPlaneGeometry(device: device)!
+        
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.scene = SCNScene()
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, SCNDebugOptions.showWireframe]
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
+        
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        configuration.planeDetection = [.vertical, .horizontal]
+        
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        print("\(self.classForCoder)/" + #function + ", anchor id: \(anchor.identifier)")
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {fatalError()}
+        planeGeometry.update(from: planeAnchor.geometry)
+        planeGeometry.materials.first?.diffuse.contents = UIColor.green.withAlphaComponent(0.3)
+        let planeNode = SCNNode(geometry: planeGeometry)
+        node.addChildNode(planeNode)
+        //        planeAnchor.addPlaneNode(on: node, geometry: planeGeometry, contents: UIColor.green.withAlphaComponent(0.3))
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {fatalError()}
+        planeGeometry.update(from: planeAnchor.geometry)
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        print("\(self.classForCoder)/" + #function)
     }
 }
